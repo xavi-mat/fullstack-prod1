3de901ad2c1c0820fb8c89feefad97bcdaf7a9f2
cmoticeac <147082946+cmoticeac@users.noreply.github.com>
2023-10-17 21:00:28 +0200

    Add files via upload

---
+++ JavaScripP1/fullstack-prod1-main/script01.js
@@ -0,0 +1,34 @@
+// JavaScript para la lógica de la interfaz
+
+// Función para abrir el modal de asignatura
+function abrirModalAsignatura() {
+    // Lógica para mostrar el modal
+}
+
+// Función para cerrar el modal de asignatura
+function cerrarModalAsignatura() {
+    // Lógica para ocultar el modal
+}
+
+// Función para agregar una asignatura al listado de asignaturas sin estado
+function agregarAsignaturaSinEstado() {
+    // Lógica para agregar una asignatura sin estado
+}
+
+// Función para mover una asignatura entre zonas de estado usando la API Drag & Drop
+function moverAsignatura(event) {
+    // Lógica para mover la asignatura
+}
+
+// Función para eliminar una asignatura
+function eliminarAsignatura() {
+    // Lógica para eliminar la asignatura (con confirmación)
+}
+
+// Puedes agregar más lógica y eventos según tus necesidades
+
+// Event listeners
+document.getElementById("btnAgregarAsignatura").addEventListener("click", abrirModalAsignatura);
+document.querySelector(".close").addEventListener("click", cerrarModalAsignatura);
+
+// Añadir más eventos según tu lógica de arrastrar y soltar, validación, etc.
